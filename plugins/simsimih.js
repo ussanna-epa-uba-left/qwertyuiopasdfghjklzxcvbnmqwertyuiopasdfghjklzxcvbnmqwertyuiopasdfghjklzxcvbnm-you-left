85ab9f43a3772b807c1572d66a0898111ec36ce7
NICO (SL Technical Tips) <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-17 11:36:05 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/simsimih.js
+++ plugins/simsimih.js
@@ -1,3 +1,19 @@
+/* Copyright (C) 2021 Vai838.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsenaDuplicated
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+
+// Description
+const BOT_DESC = "Chat with an AI Bot. \n Use .bot <message>"
+const BOT = ":"
+const NOT_FOUNDAC = "*Invaild Request*"
+const NEED_LOCATIONA = "*Invaild Request*"
+
 Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	    if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=en&cf=true`;
