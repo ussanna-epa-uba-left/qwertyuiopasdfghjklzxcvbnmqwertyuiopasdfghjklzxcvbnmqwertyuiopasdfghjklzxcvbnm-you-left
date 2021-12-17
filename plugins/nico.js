8522a1111863d15fee19058561bf73104831e49a
NICO (SL Technical Tips) <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-17 18:21:19 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/nico.js
+++ plugins/nico.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Whatsapp bot - NICO
+Cyber Queen bot - NICO
 
 */
 
@@ -11,9 +11,9 @@ const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 const cyber = ('Cyber Queen Github Link');
-const AAA = ('CYBER QUEEN GITHUB LINK')
-const AAB = "🅒🅨🅑🅔🅡 🅠🅤🅔🅔🅝 Github link එක ඉහත ඇත,"
-const AAC = "⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ WhatsApp BOT 1.0v BETA Edition Available now!💃🏻♥️ \n\nGithub :\nhttps://github.com/SLTechnicalTips/CyberQueen\n\nInstall bot : https://heroku.com/deploy?template=https://github.com/SLTechnicalTips/CyberQueen\n\nQR Code :\nhttps://replit.com/@NICONico6/CyberQueen-QR\n⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎"
+const AAA = ('CYBER QUEEN WA BOT LINKS')
+const AAB = "🅒🅨🅑🅔🅡 🅠🅤🅔🅔🅝 links එක ඉහත ඇත,"
+const AAC = "⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ WhatsApp BOT 1.0v BETA Edition Available now!💃🏻♥️ \n\nGithub Link :\nhttps://github.com/SLTechnicalTips/CyberQueen\n\nInstall bot : https://heroku.com/deploy?template=https://github.com/SLTechnicalTips/CyberQueen\n\nQR Code :\nhttps://replit.com/@NICONico6/CyberQueen-QR\n⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎"
 Asena.addCommand({pattern: 'git', fromMe: true, delowndinukacmd: false, desc: AAA,}, (async (message, match) => {
 
   var msg = await message.reply('CYBER QUEEN GITHUB LINK');
