04f80acd9572b49259de7b0de042113e3f34d3d8
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-31 12:14:58 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,10 +1,10 @@
-/* Copyright (C) 2022
-
+/* Copyright (C) 2021 Queen Amdi.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
+Queen Amdi - Black Amda
 */
 
-const Cyber = require('../events');
+const Amdi = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
 const Config = require('../config');
@@ -15,21 +15,20 @@ let LOL = Config.WORKTYPE == 'public' ? false : true
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
-const RULE_DESC = "කණ්ඩායම් පරිපාලකයින් විසින් නියම කරන ලද නීති පෙන්වයි."
-const rules = "*පරිපාලකයින් විසින් නියම කරන ලද නීති නොමැත.*"
 
-Cyber.applyCMD({pattern: 'rules', fromMe: LOL,  deleteCommand: false, onlyGroup: true, desc: RULE_DESC}, (async (message, match) => {
+
+Amdi.applyCMD({pattern: 'rules', fromMe: LOL,  deleteCommand: false, onlyGroup: true, desc: Lang.RULE_DESC}, (async (message, match) => {
 
     if (Config.RULES == 'default') {
             
-        var image = await axios.get (Config.RULES_LOGO, {responseType: 'arraybuffer'})
+        var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
        
-    await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '⚜️ *⟪Group Rules⟫* ⚜️\n\n' + rules})
+    await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '⚜️ *⟪Group Rules⟫* ⚜️\n\n' + Lang.rules})
 
     }
     else {
             
-        var image = await axios.get (Config.RULES_LOGO, {responseType: 'arraybuffer'})
+        var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
        
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '⚜️ *⟪Group Rules⟫* ⚜️\n\n' + Config.RULES })
     }
