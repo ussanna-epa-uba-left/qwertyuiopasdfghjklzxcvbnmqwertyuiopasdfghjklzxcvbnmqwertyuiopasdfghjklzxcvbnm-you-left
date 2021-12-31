8b75aeaa1b947f45e8c0d3cbf1b115bddb9dd884
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-31 14:40:46 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -1,36 +1,17 @@
-
-const Asena = require('../events');
+const CyberQueen = require('../events');
+const TAGADMİN = "Tags group admins."
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
-const Language = require('../language');
-const Lang = Language.getString('tagall');
-
-if (Config.WORKTYPE == 'private') {
-    Asena.applyCMD({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN,  deleteCommand: false}, (async (message, match) => {
+    CyberQueen.addCommand({pattern: 'tagadmin', fromMe: false, desc: TAGADMİN}, (async (message, match) => {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += ' @' + uye.id.split('@')[0] + '\n';
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
-        await message.client.sendMessage(message.jid, '᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴜᴇᴇɴᬐ\n' + '║ *This Group Admin List*\n' + '\n' + mesaj + ' \n' + 'Copyright © 2021 | ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ', MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }));
-}
-else if (Config.WORKTYPE == 'public') {
-    Asena.applyCMD({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
-        let grup = await message.client.groupMetadata(message.jid);
-        var jids = [];
-        mesaj = '';
-        grup['participants'].map(async (uye) => {
-            if (uye.isAdmin) {
-                mesaj += ' @' + uye.id.split('@')[0] + '\n';
-                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-            }
-        });
-        await message.client.sendMessage(message.jid, '᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴜᴇᴇɴᬐ\n' + '║ *This Group Admin List*\n' + '║ \n' + mesaj + ' \n' + 'Copyright © 2021 | ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ', MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-    }));
-}
