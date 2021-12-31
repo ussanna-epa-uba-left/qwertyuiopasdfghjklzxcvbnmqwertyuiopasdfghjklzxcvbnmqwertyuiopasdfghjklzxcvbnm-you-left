3962551e3466645b856cb17cfb1f740a9bbbd64c
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-31 15:42:13 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -1,17 +1,40 @@
-const CyberQueen = require('../events');
-const TAGADMİN = "Tags group admins."
+/* Codded by @phaticusthiccy & @MrJoka-Thejaka
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
-    CyberQueen.addCommand({pattern: 'tagadmin', fromMe: false, desc: TAGADMİN}, (async (message, match) => {
+const Language = require('../language');
+const Lang = Language.getString('tagall');
+
+if (Config.WORKTYPE == 'private') {
+    Asena.addCommand({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                mesaj += '🎃 @' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
         await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }));
+}
+else if (Config.WORKTYPE == 'public') {
+    Asena.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '🎃 @' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
