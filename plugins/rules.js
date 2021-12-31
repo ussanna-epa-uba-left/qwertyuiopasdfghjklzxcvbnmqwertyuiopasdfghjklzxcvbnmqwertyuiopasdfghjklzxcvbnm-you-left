ae0bccbc5bcd49482738d240d3f421282a7a7222
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-31 12:19:49 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,10 +1,10 @@
-/* Copyright (C) 2021 Queen Amdi.
+/* Copyright (C) 2022
+
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Queen Amdi - Black Amda
 */
 
-const Amdi = require('../events');
+const Cyber = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
 const Config = require('../config');
@@ -17,18 +17,18 @@ const Lang = Language.getString('system_stats');
 
 
 
-Amdi.applyCMD({pattern: 'rules', fromMe: LOL,  deleteCommand: false, onlyGroup: true, desc: Lang.RULE_DESC}, (async (message, match) => {
+Cyber.applyCMD({pattern: 'rules', fromMe: LOL,  deleteCommand: false, onlyGroup: true, desc: Lang.RULE_DESC}, (async (message, match) => {
 
     if (Config.RULES == 'default') {
             
-        var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
+        var image = await axios.get (Config.RULES_LOGO, {responseType: 'arraybuffer'})
        
     await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '⚜️ *⟪Group Rules⟫* ⚜️\n\n' + Lang.rules})
 
     }
     else {
             
-        var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
+        var image = await axios.get (Config.RULES_LOGO, {responseType: 'arraybuffer'})
        
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '⚜️ *⟪Group Rules⟫* ⚜️\n\n' + Config.RULES })
     }
