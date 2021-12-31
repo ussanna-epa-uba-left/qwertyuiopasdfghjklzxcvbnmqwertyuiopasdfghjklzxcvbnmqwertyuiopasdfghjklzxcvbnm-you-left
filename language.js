ffe9139ab430ba50f2323e3065033d0ce3c1233c
✬  CYBER DRACO ✬ <95996178+CyberDraco@users.noreply.github.com>
2021-12-31 12:22:43 +0530

    Error Fix

--- language.js
+++ language.js
@@ -4,13 +4,13 @@ const chalk = require('chalk');
 
 if (fs.existsSync('./language/' + Config.LANG + '.json')) {
     console.log(
-        chalk.green.bold('Loading Sinhala language...')
+        chalk.green.bold('Loading ' + Config.LANG + ' language...')
     );
 
     var json = JSON.parse(fs.readFileSync('./language/' + Config.LANG + '.json'));
 } else {
     console.log(
-        chalk.red.bold('᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ - Beta Edition')
+        chalk.red.bold('You entered an invalid language. English language was chosen.')
     );
 
     var json = JSON.parse(fs.readFileSync('./language/EN.json'));
