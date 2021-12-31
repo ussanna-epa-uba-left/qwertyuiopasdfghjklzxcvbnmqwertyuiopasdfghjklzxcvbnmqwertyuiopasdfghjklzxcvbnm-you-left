77fd23889be70cbcf48f41974185323dce21e8c7
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-31 16:24:42 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

---
+++ plugins/ep.js
@@ -0,0 +1,18 @@
+/*Coded By Mr-Joka*/
+
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+let workt = Config.WORKTYPE == 'public' ? false : true
+const E_DESC = 'Sends an Estetic Pic.'
+
+
+Asena.addCommand({ pattern: 'epic ?(.*)', fromMe: workt, desc: E_DESC }, (async (message, match) => {
+      
+        var espic = await axios.get(`https://api.zeks.xyz/api/estetikpic?apikey=thejakamizuki`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(espic.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Copiright © | ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐɪ'})
+    
+    }));
