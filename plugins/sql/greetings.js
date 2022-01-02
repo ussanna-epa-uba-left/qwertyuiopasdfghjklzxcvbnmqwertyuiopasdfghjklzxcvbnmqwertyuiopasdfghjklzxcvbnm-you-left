97aa3400a9236ed915c7de30013b9a9c7a95c560
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-01-02 20:38:31 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/sql/greetings.js
+++ plugins/sql/greetings.js
@@ -1,9 +1,8 @@
-/* Copyright (C) 2020 Yusuf Usta. / Copyright (C) 2021 Mr.joka.
+/* Copyright (C) 2021
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Mizuki - Mr.joka
 */
 
 const config = require('../../config');
