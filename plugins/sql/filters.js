fd1691b5e87c2e4ffd937fc2732e3c31df6e8f35
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-01-02 20:38:10 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/sql/filters.js
+++ plugins/sql/filters.js
@@ -1,9 +1,8 @@
-/* Copyright (C) 2020 Maraya.
+/* Copyright (C) 2021
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Maraya - yasasdileepa
 */
 
 const config = require('../../config');
