b27aaa2cd7cdf0be132ec2c8c2d6d2d906b6819b
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-01-14 19:23:21 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- plugins/help.js
+++ plugins/help.js
@@ -1,124 +1,300 @@
-/* Codded by @phaticusthiccy
-Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
+/* Copyright (C) 2021 NICO.
 
+Licensed under the  GPL-3.0 License;
+
+you may not use this file except in compliance with the License.
+
+ 
+
+*/
 
 const Asena = require('../events');
-const {MessageType, MessageOptions} = require('@adiwajshing/baileys');
+
 const Config = require('../config');
 
-// ==================== MAIN DESCRIPTION TEXT ====================
-const h_Des = "Yardım menüsünden botu kullanım hakkında bilgi verir."
-const h_DedEN = "Gives information about using the bot from the Help menu."
-const matchnullEN = "========== *🆘 General Help 🆘* \n\n᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ\n==========\n\n🔹 *.alive:* Checks if the bot is running.\n\n🔹 *.panel:* Shows the complete list of commands.\n🔹 *.setvar:* It settings config without entering Heroku.\n\n🔸 For more help, use the command ```.help <the topic you want help with>```\nExample: ```.help how can ı turn to public my bot?```\n\n========== *End General Help* =========="
-const matchnull = "========== *🆘 Genel Yardım 🆘* ==========\n\n🔹 *.alive:* Botun çalışıp çalışmadığını kontrol eder.\n🔹 *.panel:* Tüm komut listesini gösterir.\n🔹 *.setvar:* Herokuya girmeden config ayarlar.\n\n🔸 Daha fazla yardım için ```.help <yardım almak istediğiniz konu>``` komutunu kullanın.\nÖrnek: ```.help botumu nasıl public yaparım?```\n\n========== *Genel Yardım Bitti* =========="
-const notfound = "```Almak istediğiniz yardım bulunamadı!```\n```Lütfen daha açıklayıcı bir şekilde sorunu belirtin.```"
-const notfoundEN = "```The help you wanted to get was not found!```\n```Please state the problem in a more descriptive way.```"
+const {MessageType} = require('@adiwajshing/baileys');
 
-// ==================== ALL DESCRİPTİONS ====================
-const pubTR = "Botunuzu public yapmak komutları herkese açık yapacaktır. Public yapıldıktan sonra kullanıcı sadece kişisel ve admin komutlarını kullanabilir hale gelir. Onun dışında komutları kullanamaz.\nBotunuzu public yapmak için *.setvar WORK_TYPE:public* komutunu kullanın."
-const pubEN = "Making your bot public will make the commands public. After it is made public, the user can only use personal and admin commands. User cannot use commands other than this.\n To make your bot public, type *.setvar WORK_TYPE:public*"
-const blTR = "BlockChat botunu belirlediğiniz gruba, kişiye veya birden fazla sohbete kapatır.\nKullanmak için önce sohbete gidip *.jid* yazın. Daha sonra gelen *90xxx-xxx@g.us veya 90xxx@whatsapp.net* yazısını kopyalayın. (@g.us ve @whatsapp.net'i silin!)\nArdından *.setvar BLOCK_CHAT:id && id1,id2...* komutunu kullanın."
-const sudoTR = "SUDO, botunuzu seçtiğiniz kullanıcıya tüm yetkileri ile paylaşır. Numara sonuna ,0 koyarsanız gruplarda da aktif hale gelir.\nKullanmak için *.setvar SUDO:90xxxx && 90xx,90xxx [ülke kodu ile, (❌ +90xx • ✅ 90xx)]"
+const fs = require("fs")
 
-const privTR = "Botunuzu private yapmak komutları sadece size özel yapar. Başkaları kullanamaz.\nBotunuzu private yapmak için *.setvar WORK_TYPE:private* komutunu kullanın."
-const privEN = "Making your bot private makes commands private only for you. Anyone cannot use.\nTo make your bot private, type *.setvar WORK_TYPE:private*"
-const blEN = "Closes BlockChat bot to group, person or multiple chats you specify.\n To use it, first go to chat and type *.jid* Then copy to incoming code. (Except @g.us or @whatsapp.net!)\nThen use this command *.setvar BLOCK_CHAT:id && id1,id2..*"
-const sudoEN = "SUDO, Shares your bot to the user you choose with all its powers.If you put ,0 at the end of the number, the user can also use it in the group.\nTo use, type *.setvar SUDO:90xxxx && 90xx,90xxx [with county code, (❌ +90xx • ✅ 90xx)]*"
+const asena = fs.readFileSync('./media/CyberQueen.mp3') 
 
-if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
-    
-    Asena.addCommand({pattern: 'help ?(.*)', fromMe: true, desc: h_Des}, (async (message, match) => {
+const Language = require('../language');
+
+const Lang = Language.getString('_asena');
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'help ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+
+        }
+
+        var CMD_HELP = '';
 
         if (match[1] === '') {
-            return await message.client.sendMessage(
-                message.jid,
-                matchnull,
-                MessageType.text
+
+            Asena.commands.map(
+
+                async (command) =>  {
+
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+
+                    try {
+
+                        var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+
+                    } catch {
+
+                        var match = [command.pattern];
+
+                    }
+
+    
+
+                    var HANDLER = '';
+
+    
+
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+
+                    } else {
+
+                        HANDLER = '.';
+
+                    }
+
+                    CMD_HELP += '🎃 ' + Lang.COMMAND + ': ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+
+                    if (command.desc !== '') CMD_HELP += '*👋 ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+
+                    if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+
+                    if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+                }
+
             );
-        }
-        else if ( (match[1].includes('public') && match[1].includes('nasıl')) || (match[1].includes('public') && match[1].includes('yapimi')) || (match[1].includes('public') && match[1].includes('yapımı')) || (match[1].includes('public') && match[1].includes('nedir')) || (match[1].includes('herkese') && match[1].includes('acik') || match[1].includes('açık')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                pubTR,
-                MessageType.text
+
+        
+
+            await message.client.sendMessage(
+
+                message.jid,'⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍⚍\n' + ' ```᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ Menu```\n' + '⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎\n\n' + CMD_HELP, MessageType.text,{quoted: message.data}
+
+            );    
+
+        } else {
+
+            if (message.jid === '905524317852-1612300121@g.us') {
+
+                return;
+
+            }
+
+            var CMD_HELP = '';
+
+            Asena.commands.map(
+
+                async (command) =>  {
+
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+
+                    try {
+
+                        var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+
+                    } catch {
+
+                        var cmatch = [command.pattern];
+
+                    }
+
+                
+
+                    if (cmatch[2] == match[1]) {
+
+                        var HANDLER = '';
+
+    
+
+                        if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+
+                            HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+
+                        } else {
+
+                            HANDLER = '.';
+
+                        }
+
+                        CMD_HELP += '🔱 ' + Lang.COMMAND + ': ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+
+                        if (command.desc !== '') CMD_HELP += '*⚓ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+
+                        if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+
+                        if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+                    }
+
+                }
+
             );
-        }
-        else if ( (match[1].includes('private') && match[1].includes('nasıl')) || (match[1].includes('private') && match[1].includes('yapimi')) || (match[1].includes('private') && match[1].includes('yapımı')) || (match[1].includes('private') && match[1].includes('nedir')) || (match[1].includes('bana') && match[1].includes('özel')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                privTR,
-                MessageType.text
+
+            if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+
+            await message.client.sendMessage(
+
+                message.jid,'```᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ Menu ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ```\n\n' + CMD_HELP, MessageType.text,{quoted: message.data}
+
             );
+
         }
-        else if ( (match[1].includes('belirli') && match[1].includes('sohbet')) || (match[1].includes('sohbet') && match[1].includes('kapat')) || (match[1].includes('nasil') && match[1].includes('kapatabilirim')) || (match[1].includes('blockchat') && match[1].includes('nedir')) || (match[1].includes('sohbet') && match[1].includes('özel')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                blTR,
-                MessageType.text
-            );
-        }
-        else if ( (match[1].includes('SUDO') && match[1].includes('nedir')) || (match[1].includes('SUDO') && match[1].includes('nasil')) || (match[1].includes('botu') && match[1].includes('baskasina')) || (match[1].includes('botu') && match[1].includes('arkadaşıma')) || (match[1].includes('SUDO') && match[1].includes('kullanmak')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                sudoTR,
-                MessageType.text
-            );
-        }
-        else {
-            return await message.client.sendMessage(
-                message.jid,
-                notfound,
-                MessageType.text
-            );
-        }
+
     }));
+
 }
-else {
-    
-    Asena.addCommand({pattern: 'help ?(.*)', fromMe: true, desc: h_DedEN}, (async (message, match) => {
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'help ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+
+        }
+
+        var CMD_HELP = '';
 
         if (match[1] === '') {
-            return await message.client.sendMessage(
-                message.jid,
-                matchnullEN,
-                MessageType.text
+
+            Asena.commands.map(
+
+                async (command) =>  {
+
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+
+                    try {
+
+                        var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+
+                    } catch {
+
+                        var match = [command.pattern];
+
+                    }
+
+    
+
+                    var HANDLER = '';
+
+    
+
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+
+                    } else {
+
+                        HANDLER = '.';
+
+                    }
+
+                    CMD_HELP += '🔱 ' + Lang.COMMAND + ': ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+
+                    if (command.desc !== '') CMD_HELP += '*⚓ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+
+                    if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+
+                    if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+                }
+
             );
-        }
-        else if ( (match[1].includes('public') && match[1].includes('how')) || (match[1].includes('public') && match[1].includes('set')) || (match[1].includes('public') && match[1].includes('setting')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                pubEN,
-                MessageType.text
+
+        
+
+            await message.client.sendMessage(
+
+                message.jid,'🔱᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ PUBLIC🔱\n\n' + CMD_HELP, MessageType.text,{quoted: message.data}
+
+            );    
+
+        } else {
+
+            if (message.jid === '905524317852-1612300121@g.us') {
+
+                return;
+
+            }
+
+            var CMD_HELP = '';
+
+            Asena.commands.map(
+
+                async (command) =>  {
+
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+
+                    try {
+
+                        var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+
+                    } catch {
+
+                        var cmatch = [command.pattern];
+
+                    }
+
+                
+
+                    if (cmatch[2] == match[1]) {
+
+                        var HANDLER = '';
+
+    
+
+                        if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+
+                            HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+
+                        } else {
+
+                            HANDLER = '.';
+
+                        }
+
+                        CMD_HELP += '🔱 ' + Lang.COMMAND + ': ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+
+                        if (command.desc !== '') CMD_HELP += '*⚓ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+
+                        if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+
+                        if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+                    }
+
+                }
+
             );
-        }
-        else if ( (match[1].includes('private') && match[1].includes('how')) || (match[1].includes('private') && match[1].includes('set')) || (match[1].includes('private') && match[1].includes('setting')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                privEN,
-                MessageType.text
+
+            if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+
+            await message.client.sendMessage(
+
+                message.jid,'🔱᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ PUBLIC🔱\n\n' + CMD_HELP, MessageType.text,{quoted: message.data}
+
             );
+
         }
-        else if ( (match[1].includes('SUDO') && match[1].includes('usage')) || (match[1].includes('SUDO') && match[1].includes('what')) || (match[1].includes('how') && match[1].includes('SUDO')) || (match[1].includes('set') && match[1].includes('SUDO')) || (match[1].includes('share') && match[1].includes('bot')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                sudoEN,
-                MessageType.text
-            );
-        }
-        else if ( (match[1].includes('block') && match[1].includes('chat')) || (match[1].includes('how') && match[1].includes('block')) || (match[1].includes('close') && match[1].includes('bot')) || (match[1].includes('specific') && match[1].includes('chat')) || (match[1].includes('specific') && match[1].includes('set')) ) {
-            return await message.client.sendMessage(
-                message.jid,
-                blEN,
-                MessageType.text
-            );
-        }
-        else {
-            return await message.client.sendMessage(
-                message.jid,
-                notfoundEN,
-                MessageType.text
-            );
-        }
+
     }));
-} */
+
+}
+
