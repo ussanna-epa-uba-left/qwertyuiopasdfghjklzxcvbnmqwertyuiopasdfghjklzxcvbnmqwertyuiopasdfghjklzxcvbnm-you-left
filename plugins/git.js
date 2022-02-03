dfb712f0a63a7951b3abe6d285ae50de96cb6a69
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-02-03 08:53:01 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴜᴇᴇɴᬐ

--- plugins/git.js
+++ plugins/git.js
@@ -12,9 +12,9 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 const cyber = ('Cyber Queen Github Link');
 const AAA = ('CYBER QUEEN WA BOT LINKS')
-const AAB = "🅒🅨🅑🅔🅡 🅠🅤🅔🅔🅝 links ඉහත ඇත,"
-const AAC = "ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴄʏʙᴇʀ ǫᴜᴇᴇɴ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ɴᴏᴡ ᴅᴇᴘʟᴏʏ ᴠɪᴀ ᴛʜɪꜱ ʟɪɴᴋꜱ 💃♥️\n᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ \n\nɢɪᴛʜᴜʙ ʟɪɴᴋ:\nhttps://github.com/SLTechnicalTips/CyberQueen\n\nɪɴꜱᴛᴀʟʟ ʙᴏᴛ:\nhttps://heroku.com/deploy?template=https://github.com/SLTechnicalTips/CyberQueen\n\nǫʀ ᴄᴏᴅᴇ ʟɪɴᴋ:\nhttps://replit.com/@NICONico6/CyberQueen-QR\n\nᴄᴏᴘʏʀɪɢʜᴛ © | ᴄʏʙᴇʀ ǫᴜᴇᴇɴ\n↝↢↤⟲⟲⟲⤛⤛⤜⤜⟳⟳⟳↦↣↜"
-Asena.addCommand({pattern: 'git', fromMe: true, delowndinukacmd: false, desc: AAC,}, (async (message, match) => {
+const AAB = "🅒🅨🅑🅔🅡 🅠🅤🅔🅔🅝 links,"
+const AAC = "💃♥️\n᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ\n\nɢɪᴛʜᴜʙ ʟɪɴᴋ:\nhttps://github.com/SLTechnicalTips/CyberQueen\n\nɪɴꜱᴛᴀʟʟ ʙᴏᴛ:\nhttps://heroku.com/deploy?template=https://github.com/SLTechnicalTips/CyberQueen\n\nǫʀ ᴄᴏᴅᴇ ʟɪɴᴋ:\nhttps://replit.com/@NICONico6/CyberQueen-QR\n\nᴄᴏᴘʏʀɪɢʜᴛ © | ᴄʏʙᴇʀ ǫᴜᴇᴇɴ\n↝↢↤⟲⟲⟲⤛⤛⤜⤜⟳⟳⟳↦↣↜"
+Asena.addCommand({pattern: 'git', fromMe: true, delowndinukacmd: false, desc: AAB,}, (async (message, match) => {
 
   var msg = await message.reply('CYBER QUEEN GITHUB LINK');
                                      
