b6ae0cbd5a97054a2425fcb92367c86d01200664
✬  CYBER DRAXO ✬ <99461944+CyberDraxo@users.noreply.github.com>
2022-02-16 20:05:14 +0530

    ```💃 CQ DEV = Nico 💃```

--- plugins/Dev-1.js
+++ plugins/Dev-1.js
@@ -1,17 +1,16 @@
-/*const Cyber = require('../events');
+const Cyber = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const COE = ('VCARD')
+let Work_Mode = Config.WORKTYPE == 'public' ? false : true
 
 Cyber.addCommand({pattern: 'dev1', fromMe: Work_Mode, desc: COE,  deleteCommand: false}, (async (message, match) => {
-
-
+ 
+            
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
             + 'FN:♦️⇝͜͡✪ஓீ͜ɴͥɪᴄᷧᴏᷟ࿐♠⁩ \n' 
             + 'ORG:Cyber Queen;\n'
             + 'TEL;type=CELL;type=VOICE;waid=94761219834:+94 76 121 9834\n' 
             + 'END:VCARD'
-
 await message.client.sendMessage(message.jid, {displayname: "♦️⇝͜͡✪ஓீ͜ɴͥɪᴄᷧᴏᷟ࿐♠⁩ ", vcard: vcard}, MessageType.contact)
-
- }*/
+ })
