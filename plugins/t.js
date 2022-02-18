bc51d96432d1dfac3ae18169f3c3b4b6416946c5
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-02-18 13:28:07 +0530

    .

--- plugins/t.js
+++ plugins/t.js
@@ -25,6 +25,8 @@ const Glang = Language.getString('github');
 const Slang = Language.getString('lyrics');
 const Clang = Language.getString('covid');
 
+if (config.WORKTYPE == 'private') {
+
 cobra.addCommand({pattern: 'play ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text);    
@@ -53,6 +55,79 @@ cobra.addCommand({pattern: 'play ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (a
                     });
                 writer.addTag();
                 const msg = '⇢ ' + ':' + title + '\n\n' + '⇢ :Type : Document' + '\n\n' +'```Please Wait...```'
+                var iavatar = await axios.get(thumbnail,{responseType: 'arraybuffer'});
+                reply = await message.client.sendMessage(message.jid,config.SONG_UP, MessageType.text);
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg', ptt: false});
+                
+            });
+    }));
+
+    
+    else if (config.WORKTYPE == 'public') {
+        
+            cobra.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: Lang.SONG_DESC}, (async (message, match) => { 
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text);    
+        let arama = await yts(match[1]);
+        arama = arama.all;
+        if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
+        var reply = await message.client.sendMessage(message.jid,config.SONG_DOWN, MessageType.text);
+        let thumbnail = arama[0].thumbnail.replace(' ', '+');
+        let title = arama[0].title.replace(' ', '+');
+        let stream = ytdl(arama[0].videoId, {
+            quality: 'lowestaudio',
+        });
+    
+        got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
+        ffmpeg(stream)
+            .audioBitrate(320)
+            .save('./' + title + '.mp3')
+            .on('end', async () => {
+                const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
+                writer.setFrame('TIT2', arama[0].title)
+                    .setFrame('TPE1', [arama[0].author.name])
+                    .setFrame('APIC', {
+                        type: 3,
+                        data: fs.readFileSync(title + '.jpg'),
+                        description: arama[0].description
+                    });
+                writer.addTag();
+                const msg = '⇢ ' + ':' + title + '\n\n' + '⇢ :Type : MP3' + '\n\n' +'```Please Wait...```'
+                var iavatar = await axios.get(thumbnail,{responseType: 'arraybuffer'});
+                reply = await message.client.sendMessage(message.jid,config.SONG_UP, MessageType.text);
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg', ptt: false});
+               
+            });
+    }));
+    
+    cobra.addCommand({pattern: 'play ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (async (message, match) => { 
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text);    
+        let arama = await yts(match[1]);
+        arama = arama.all;
+        if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
+        var reply = await message.client.sendMessage(message.jid,config.SONG_DOWN, MessageType.text);
+        let thumbnail = arama[0].thumbnail.replace(' ', '+');
+        let title = arama[0].title.replace(' ', '+');
+        let stream = ytdl(arama[0].videoId, {
+            quality: 'lowestaudio',
+        });
+    
+        got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
+        ffmpeg(stream)
+            .audioBitrate(320)
+            .save('./' + title + '.mp3')
+            .on('end', async () => {
+                const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
+                writer.setFrame('TIT2', arama[0].title)
+                    .setFrame('TPE1', [arama[0].author.name])
+                    .setFrame('APIC', {
+                        type: 3,
+                        data: fs.readFileSync(title + '.jpg'),
+                        description: arama[0].description
+                    });
+                writer.addTag();
+                const msg = '⇢ ' + ':' + title + '\n\n' + '⇢ :Type : MP3' + '\n\n' +'```Please Wait...```'
                 var iavatar = await axios.get(thumbnail,{responseType: 'arraybuffer'});
                 reply = await message.client.sendMessage(message.jid,config.SONG_UP, MessageType.text);
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg', ptt: false});
